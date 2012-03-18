0e709f1660a5fc2d767fc9365bfdf25b76342663
John Glassmyer <john.glassmyer@gmail.com>
2012-03-17 22:56:25 -0700

    haskell/day1 progress

---
+++ haskell/day1/self_study.hs
@@ -0,0 +1,34 @@
+module Main where
+	-- Write a function that takes a list and returns the same list in reverse.
+	reverseList :: [t] -> [t]
+	reverseList xs = reversePart xs []
+	
+	reversePart :: [t] -> [t] -> [t]
+	reversePart [] sx = sx
+	reversePart xs sx = reversePart (tail xs) (head xs : sx)
+	
+	-- Write a function that builds two-tuples with all possible combinations
+	-- with two of the colors black, white, blue, yellow, and red.  Note that you
+	-- should include only one of  (black, blue) and (blue, black).
+	pickTwos :: [[Char]] -> [([Char], [Char])]
+	pickTwos [] = []
+	pickTwos (h:t) = pickPairs h t ++ pickTwos t
+	
+	pickPairs :: [Char] -> [[Char]] -> [([Char], [Char])]
+	pickPairs first [] = []
+	pickPairs first seconds = [ (first, second) | second <- seconds ]
+	
+	colors = ["black", "white", "blue", "yellow", "red"]
+	colorPairs = pickTwos colors
+	
+	-- Write a list comprehension to build a childhood multiplication table.
+	-- The table would be a list of three-tuples where the first two are integers
+	-- from 1-12 and the third is the product of the first two.
+	multTable :: [Integer] -> [(Integer, Integer, Integer)]
+	multTable numbers = [ (x, y, x * y) | x <- numbers, y <- numbers ]
+	
+	multTable1to12 = multTable [1..12]
+	
+	-- Solve the map-coloring problem (Map Coloring, on page 83) using Haskell.
+	-- ...
+	
